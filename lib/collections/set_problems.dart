702f9c92e67c25a37c832f08a2536c07a6b9ee21
motabbir006 <mhsourov006@gmail.com>
2026-02-02 15:27:02 +0600

    set

--- lib/collections/set_problems.dart
+++ lib/collections/set_problems.dart
@@ -1,3 +1,12 @@
-void main() {
-  // Set practice
+void main(){
+  var nums={1,2,3,4,5,6,7,8,9,1,2,4,5,6}; //remove duplicate value auto
+  print(nums.length);
+  print(nums);
+  print(nums.isEmpty);
+  print(nums.isNotEmpty);
+  nums.add(12);
+  nums.remove(2);
+   var uniqe = nums.toSet().toList();//Convert set to list
+   print(uniqe);
+
 }
